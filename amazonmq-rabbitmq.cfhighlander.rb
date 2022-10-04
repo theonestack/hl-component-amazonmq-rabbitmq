bc91b55772f012b021e8ef28092de3e025c57228
Aaron Walker <a.walker@base2services.com>
2022-10-04 15:46:00 +0000

    remove EngineVersion allow values to support any supported rabbitmq version

--- amazonmq-rabbitmq.cfhighlander.rb
+++ amazonmq-rabbitmq.cfhighlander.rb
@@ -8,7 +8,7 @@ CfhighlanderTemplate do
     ComponentParam 'EnvironmentName', 'dev', isGlobal: true
     ComponentParam 'EnvironmentType', 'development', allowedValues: ['development','production'], isGlobal: true
     ComponentParam 'DeploymentMode', 'SINGLE_INSTANCE', allowedValues: ['SINGLE_INSTANCE', 'CLUSTER_MULTI_AZ']
-    ComponentParam 'EngineVersion', '3.8.6', allowedValues: ['3.8.6']
+    ComponentParam 'EngineVersion', '3.8.6'
     ComponentParam 'HostInstanceType', 'mq.t3.micro', allowedValues: ['mq.t3.micro', 'mq.m5.large', 'mq.m5.xlarge', 'mq.m5.2xlarge', 'mq.m5.4xlarge']
     ComponentParam 'VPCId', type: 'AWS::EC2::VPC::Id'
     ComponentParam 'SubnetIds', type: 'CommaDelimitedList'
